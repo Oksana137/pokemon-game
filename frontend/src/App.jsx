338f56517e3f4a1b759e79b71c1625b194d1e073
Oksana <axana13@gmail.com>
2024-10-09 18:50:41 +0200

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,21 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesElements,
+} from "react-router-dom";
+
 function App() {
-  return <></>;
+  const router = createBrowserRouter(
+    createRoutesElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Cards />} />
+        <Route path="/:id" element={<Detail />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+const NavBar = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Link</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
